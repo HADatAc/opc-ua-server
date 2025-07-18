2efa037f7887d50ecd07b0bfd039009cbffb5b86
root <root@ip-172-31-37-97.eu-west-1.compute.internal>
2025-07-18 15:49:57 +0000

    updates

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV '/home/tiago-moreira/Secretária/git-hascorepo/opc-ua-server/opc-ua-server/venv'
+set -gx VIRTUAL_ENV /home/ubuntu/opc-ua-server/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
